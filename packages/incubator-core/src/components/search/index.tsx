05ddd9c3f0bb63bde817f1b2a15d2eca6774046d
andres <andres@doersdf.com>
2023-02-15 11:50:26 +0100

    chore: use vite instead of tsup for building the library

--- packages/incubator-core/src/components/search/index.tsx
+++ packages/incubator-core/src/components/search/index.tsx
@@ -1,7 +1,8 @@
-import React, { FC, RefAttributes } from 'react'
+import { FC, RefAttributes } from 'react'
 
 import { clsx } from 'clsx'
 
+import icon from '../../assets/icons/search-icon.svg'
 import { TextInput, TextInputProps } from '../text-input'
 
 import s from './styles.modules.scss'
@@ -9,5 +10,10 @@ import s from './styles.modules.scss'
 export type SearchProps = {} & TextInputProps & RefAttributes<HTMLInputElement>
 
 export const Search: FC<SearchProps> = ({ placeholder = 'Поиск...', className, ...rest }) => {
-  return <TextInput placeholder={placeholder} className={clsx(className, s.search)} {...rest} />
+  return (
+    <>
+      <TextInput placeholder={placeholder} className={clsx(className, s.search)} {...rest} />
+      <img src={icon} />
+    </>
+  )
 }
---
+++ packages/incubator-core/vite.config.ts
@@ -0,0 +1,30 @@
+import { resolve } from 'path'
+
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, 'src/index.tsx'),
+      name: '@incubator/core',
+      // the proper extensions will be added
+      fileName: 'index',
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['react'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'React',
+        },
+      },
+    },
+  },
+})

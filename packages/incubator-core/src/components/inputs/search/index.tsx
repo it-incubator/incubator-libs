60cc0d8fab2d6f5bd7840138761d80033d8a6624
andres <andres@doersdf.com>
2023-02-16 22:46:26 +0100

    refactor search

--- packages/incubator-core/src/components/inputs/search/index.tsx
+++ packages/incubator-core/src/components/inputs/search/index.tsx
@@ -9,5 +9,9 @@ import s from './search.module.scss'
 export type SearchProps = {} & TextFieldProps & RefAttributes<HTMLInputElement>
 
 export const Search: FC<SearchProps> = ({ placeholder = 'Поиск...', className, ...rest }) => {
-  return <TextField placeholder={placeholder} className={clsx(s.search, className)} {...rest} />
+  const classNames = {
+    search: clsx(s.search, className),
+  }
+
+  return <TextField placeholder={placeholder} className={classNames.search} {...rest} />
 }

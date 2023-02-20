df427b4658b0656290cbc51865da81fcd3ef13bc
Starkov Sergey <starkovsr@gmail.com>
2023-02-20 21:50:41 +0300

    add basic table components (#4)
    add Table component

---
+++ packages/incubator-core/src/components/table/index.tsx
@@ -0,0 +1,62 @@
+import { ComponentProps, FC, ReactNode } from 'react'
+
+import { clsx } from 'clsx'
+
+import s from './table.module.scss'
+
+export type RootProps = ComponentProps<'table'>
+
+export const Root: FC<RootProps> = ({ className, ...rest }) => {
+  const classNames = {
+    table: clsx(className, s.table),
+  }
+
+  return <table className={classNames.table} {...rest} />
+}
+
+export type HeadProps = ComponentProps<'thead'>
+
+export const Head: FC<HeadProps> = props => {
+  return <thead {...props} />
+}
+
+export type BodyProps = ComponentProps<'tbody'>
+
+export const Body: FC<BodyProps> = props => {
+  return <tbody {...props} />
+}
+
+export type RowProps = ComponentProps<'tr'>
+
+export const Row: FC<RowProps> = props => {
+  return <tr {...props} />
+}
+
+export type HeadCellProps = ComponentProps<'th'>
+
+export const HeadCell: FC<HeadCellProps> = ({ className, ...rest }) => {
+  const classNames = {
+    headCell: clsx(className, s.headCell),
+  }
+
+  return <th className={classNames.headCell} {...rest} />
+}
+
+export type CellProps = ComponentProps<'td'>
+
+export const Cell: FC<CellProps> = ({ className, ...rest }) => {
+  const classNames = {
+    cell: clsx(className, s.cell),
+  }
+
+  return <td className={classNames.cell} {...rest} />
+}
+
+export const Table = {
+  Root,
+  Head,
+  Body,
+  Row,
+  HeadCell,
+  Cell,
+}

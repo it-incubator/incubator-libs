69f2b0244f3b1561aaf35be390085484b948c05d
Starkov Sergey <starkovsr@gmail.com>
2023-03-07 18:34:20 +0300

    add interactive styles for Colllapsible

--- packages/ui-kit/stories/components/disclosure/collapsible/collapsible.stories.tsx
+++ packages/ui-kit/stories/components/disclosure/collapsible/collapsible.stories.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 import { Meta } from '@storybook/react'
 
 import { Collapsible } from '../../../../src'
@@ -10,10 +12,67 @@ export default {
 
 export const DefaultUncontrolled = {
   args: {
-    title: 'Accordion',
-    children: 'some content',
+    title: 'Collapsible',
+    children:
+      'Эффективно обучаем frontend и backend-разработке до трудоустройства, а также проводим учебную стажировку для QA, PM (project manager), front-end (React) и back-end (NestJS) специалистов.\n',
     iconComponent: <InfoIcon size={16} />,
     disabled: false,
+    description: 'Some description',
+  },
+}
+
+export const DefaultControlled = {
+  render: args => {
+    const [open, setOpen] = useState(false)
+
+    return (
+      <Collapsible open={open} onOpenChange={setOpen} title="Collapsible">
+        Эффективно обучаем frontend и backend-разработке до трудоустройства, а также проводим
+        учебную стажировку для QA, PM (project manager), front-end (React) и back-end (NestJS)
+        специалистов.
+      </Collapsible>
+    )
+  },
+}
+
+export const DefaultWithDescription = {
+  args: {
+    title: 'Collapsible',
+    children:
+      'Эффективно обучаем frontend и backend-разработке до трудоустройства, а также проводим учебную стажировку для QA, PM (project manager), front-end (React) и back-end (NestJS) специалистов.\n',
+    disabled: false,
+    description: 'Some description',
+  },
+}
+
+export const DefaultWithIcon = {
+  args: {
+    title: 'Collapsible',
+    children:
+      'Эффективно обучаем frontend и backend-разработке до трудоустройства, а также проводим учебную стажировку для QA, PM (project manager), front-end (React) и back-end (NestJS) специалистов.\n',
+    iconComponent: <InfoIcon size={16} />,
+    disabled: false,
+  },
+}
+
+export const DefaultWithDescriptionAndIcon = {
+  args: {
+    title: 'Collapsible',
+    children:
+      'Эффективно обучаем frontend и backend-разработке до трудоустройства, а также проводим учебную стажировку для QA, PM (project manager), front-end (React) и back-end (NestJS) специалистов.\nnt',
+    iconComponent: <InfoIcon size={16} />,
+    disabled: false,
+    description: 'Some description',
+  },
+}
+
+export const DefaultDisabled = {
+  args: {
+    title: 'Collapsible',
+    children:
+      'Эффективно обучаем frontend и backend-разработке до трудоустройства, а также проводим учебную стажировку для QA, PM (project manager), front-end (React) и back-end (NestJS) специалистов.\n',
+    iconComponent: <InfoIcon size={16} />,
+    disabled: true,
     description: 'Some description',
   },
 }

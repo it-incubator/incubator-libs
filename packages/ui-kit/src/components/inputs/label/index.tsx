eebadb1562a2b11ebd4ef7d3eb8a1cb0cc263f8e
Starkov Sergey <starkovsr@gmail.com>
2023-03-08 13:24:14 +0300

    fix label margin

--- packages/ui-kit/src/components/inputs/label/index.tsx
+++ packages/ui-kit/src/components/inputs/label/index.tsx
@@ -1,6 +1,7 @@
 import { ComponentPropsWithoutRef, FC } from 'react'
 
 import * as LabelRadixUI from '@radix-ui/react-label'
+import { clsx } from 'clsx'
 
 import s from './label.module.scss'
 
@@ -8,9 +9,9 @@ export type LabelProps = {
   label?: string
 } & ComponentPropsWithoutRef<'label'>
 
-export const Label: FC<LabelProps> = ({ label, children, ...rest }) => {
+export const Label: FC<LabelProps> = ({ label, children, className, ...rest }) => {
   const classNames = {
-    label: s.label,
+    label: clsx(s.label),
   }
 
   return (

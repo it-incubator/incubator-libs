4124fd88f416a3e5b1a30219a694d95af4179139
Starkov Sergey <starkovsr@gmail.com>
2023-03-16 13:27:24 +0300

    add scrollbar component

---
+++ packages/ui-kit/stories/components/scrollbar/scrollbar.stories.tsx
@@ -0,0 +1,50 @@
+import { Meta } from '@storybook/react'
+
+import { Scrollbar } from '../../../src'
+
+export default {
+  title: 'Components/Scrollbar',
+  component: Scrollbar,
+  argTypes: {
+    type: {
+      options: ['auto', 'always', 'scroll', 'hover'],
+      control: { type: 'radio' },
+    },
+  },
+} as Meta<typeof Scrollbar>
+
+export const VerticalScrollbar = {
+  args: {
+    style: { height: '200px', width: 'fit-content' },
+    children: (
+      <p style={{ width: '100px' }}>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, autem, beatae debitis,
+        earum maiores nam omnis perspiciatis quas quasi ratione tenetur voluptas voluptatem!
+        Blanditiis ducimus, excepturi iste iure quos veritatis. Lorem ipsum dolor sit amet,
+        consectetur adipisicing elit. Adipisci, autem, beatae debitis, earum maiores nam omnis
+        perspiciatis quas quasi ratione tenetur voluptas voluptatem! Blanditiis ducimus, excepturi
+        iste iure quos veritatis. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Adipisci, autem, beatae debitis, earum maiores nam omnis perspiciatis quas quasi ratione
+        tenetur voluptas voluptatem! Blanditiis ducimus, excepturi iste iure quos veritatis.
+      </p>
+    ),
+  },
+}
+
+export const HorizontalScrollbar = {
+  args: {
+    style: { width: '100px' },
+    children: (
+      <p style={{ width: '400px' }}>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, autem, beatae debitis,
+        earum maiores nam omnis perspiciatis quas quasi ratione tenetur voluptas voluptatem!
+        Blanditiis ducimus, excepturi iste iure quos veritatis. Lorem ipsum dolor sit amet,
+        consectetur adipisicing elit. Adipisci, autem, beatae debitis, earum maiores nam omnis
+        perspiciatis quas quasi ratione tenetur voluptas voluptatem! Blanditiis ducimus, excepturi
+        iste iure quos veritatis. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Adipisci, autem, beatae debitis, earum maiores nam omnis perspiciatis quas quasi ratione
+        tenetur voluptas voluptatem! Blanditiis ducimus, excepturi iste iure quos veritatis.
+      </p>
+    ),
+  },
+}

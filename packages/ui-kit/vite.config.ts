fe9ae186e835a669988139e7c21ed94c2c1401ad
andres <andres99.dev@gmail.com>
2023-12-03 12:33:10 +0100

    feat: externalize all deps

--- packages/ui-kit/vite.config.ts
+++ packages/ui-kit/vite.config.ts
@@ -1,30 +1,34 @@
 import { resolve } from 'path'
 
+import { dependencies, devDependencies, peerDependencies } from './package.json'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-
-import { peerDependencies, dependencies } from './package.json'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   build: {
     lib: {
       // Could also be a dictionary or array of multiple entry points
       entry: resolve(__dirname, 'src/index.ts'),
-      name: '@it-incubator/ui-kit',
       // the proper extensions will be added
       fileName: 'index',
       formats: ['es'],
+      name: '@it-incubator/ui-kit',
     },
     rollupOptions: {
-      external: [...Object.keys(peerDependencies), ...Object.keys(dependencies)],
+      external: [
+        ...Object.keys(peerDependencies),
+        ...Object.keys(dependencies),
+        ...Object.keys(devDependencies),
+        'react/jsx-runtime',
+      ],
       output: {
         dir: 'dist',
-        format: 'es',
         entryFileNames: '[name].js',
+        format: 'es',
       },
     },
-    target: 'esnext',
     sourcemap: true,
+    target: 'esnext',
   },
+  plugins: [react()],
 })

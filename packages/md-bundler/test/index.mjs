dd110d50cd11318e4a6f7ad812553475db35e2b0
Andres <andres99.dev@gmail.com>
2024-01-08 15:18:47 +0100

    fix: li styles

--- packages/md-bundler/test/index.mjs
+++ packages/md-bundler/test/index.mjs
@@ -7,6 +7,6 @@ process.env.NODE_ENV = 'production'
 const source = ``
 
 
-const result = await bundleMdx(source, 'C:/Users/andre/dev/inc/libs/packages/md-bundler/test/test.mdx')
+const result = await bundleMdx(source, '/Users/andres/dev/work/incubator/incubator-libs/packages/md-bundler/test/test.mdx')
 
 fs.writeFileSync('../mdx-components/stories/rendering/prose/code.ts', `export const testCode = ${JSON.stringify(result)}`, 'utf-8')

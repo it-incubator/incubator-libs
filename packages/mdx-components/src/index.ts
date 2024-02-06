6e678d78f65fabce558a30304a306061d739c2dc
andres <andres99.dev@gmail.com>
2024-02-06 13:47:00 +0100

    feat(mdx-components): fix fonts

--- packages/mdx-components/src/index.ts
+++ packages/mdx-components/src/index.ts
@@ -1,7 +1,5 @@
 import { clsx } from 'clsx'
 
-import '@fontsource/roboto'
-
 import './styles/index.scss'
 export * from './assets/icons'
 export type { IconProps } from './assets/icons/IconWrapper'

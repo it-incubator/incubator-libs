2b9f6273f30f1e94250be65619f0aae5f50024ca
andres <andres99.dev@gmail.com>
2024-02-07 19:05:26 +0100

    feat(ui-kit) add icons generator with instructions feat(ui-kit) add new icons

--- packages/ui-kit/stories/components/feedback/alert/alert.stories.tsx
+++ packages/ui-kit/stories/components/feedback/alert/alert.stories.tsx
@@ -1,10 +1,9 @@
+import { Alert, CheckBox } from '../../../../src'
 import { Meta } from '@storybook/react'
 
-import { Alert, HelpOutline } from '../../../../src'
-
 export default {
-  title: 'Components/Feedback/Alert',
   component: Alert,
+  title: 'Components/Feedback/Alert',
 } as Meta<typeof Alert>
 
 export const Warning = {
@@ -24,7 +23,7 @@ export const Error = {
 export const WithCustomIcon = {
   args: {
     children: 'Для того чтобы продолжить, пройдите все шаги указанные в инструкции',
-    icon: <HelpOutline />,
+    icon: <CheckBox />,
     variant: 'warning',
   },
 }

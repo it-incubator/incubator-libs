35a5f6808da4a492129c7de85a33768385ab50b1
andres <andres99.dev@gmail.com>
2024-06-12 16:45:03 +0200

    fix: fix tabs prop types, pass down rest props

--- packages/ui-kit/src/components/tabs/index.tsx
+++ packages/ui-kit/src/components/tabs/index.tsx
@@ -1,4 +1,4 @@
-import { FC, ReactNode } from 'react'
+import { ComponentPropsWithoutRef, FC, ReactNode } from 'react'
 
 import * as TabsRadixUI from '@radix-ui/react-tabs'
 import { clsx } from 'clsx'
@@ -15,18 +15,12 @@ export type TabType = {
 type CommonProps = {
   /** Use TabsContent components as children. */
   children?: ReactNode
-  /** The value of the tab that should be active when initially rendered. Use when you do not need to control the state of the tabs. */
-  defaultValue?: string
-  /** Event handler called when the value changes.  */
-  onValueChange?: (value: string) => void
   /** An array of objects with the value and title of the tab.
    *  {value: string, title: string}
    * */
   tabs: TabType[]
-  /** The controlled value of the tab to activate. Should be used in conjunction with onValueChange */
-  value?: string
   variant?: 'primary' | 'secondary'
-}
+} & ComponentPropsWithoutRef<typeof TabsRadixUI.Root>
 
 type ConditionalProps =
   | {
@@ -42,26 +36,20 @@ export type TabsProps = CommonProps & ConditionalProps
 
 export const Tabs: FC<TabsProps> = ({
   children,
-  defaultValue,
+  className,
   fullWidth,
-  onValueChange,
   tabs,
-  value,
   variant = 'primary',
+  ...rest
 }) => {
   const classNames = {
     list: clsx(s.list, s[variant]),
-    root: s.root,
+    root: clsx(s.root, className),
     trigger: clsx(s.trigger, fullWidth && s.fullWidth, s[variant]),
   }
 
   return (
-    <TabsRadixUI.Root
-      className={classNames.root}
-      defaultValue={defaultValue}
-      onValueChange={onValueChange}
-      value={value}
-    >
+    <TabsRadixUI.Root className={classNames.root} {...rest}>
       <TabsRadixUI.List className={classNames.list}>
         {tabs.map(tab => (
           <TabsRadixUI.Trigger

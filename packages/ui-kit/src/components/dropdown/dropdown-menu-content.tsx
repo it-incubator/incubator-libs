f7f2b2702fc778a63f095ded997cfc1b30a57f84
andres <andres99.dev@gmail.com>
2024-06-16 14:36:33 +0200

    fix header and loader stories

--- packages/ui-kit/src/components/dropdown/dropdown-menu-content.tsx
+++ packages/ui-kit/src/components/dropdown/dropdown-menu-content.tsx
@@ -7,23 +7,38 @@ import s from './dropdown-menu.module.scss'
 
 const DropdownMenuContent = forwardRef<
   ElementRef<typeof Content>,
-  ComponentPropsWithoutRef<typeof Content>
->(({ children, className, loop = true, sideOffset = 8, ...props }, ref) => (
-  <Portal>
-    <Content
-      className={clsx(s.content, className)}
-      loop={loop}
-      ref={ref}
-      sideOffset={sideOffset}
-      {...props}
-    >
-      <Arrow asChild className={s.arrowBox}>
-        <div className={s.arrow} />
-      </Arrow>
-      {children}
-    </Content>
-  </Portal>
-))
+  ComponentPropsWithoutRef<typeof Content> & ComponentPropsWithoutRef<typeof Portal>
+>(
+  (
+    {
+      align = 'end',
+      children,
+      className,
+      container,
+      forceMount,
+      loop = true,
+      sideOffset = 8,
+      ...props
+    },
+    ref
+  ) => (
+    <Portal container={container} forceMount={forceMount}>
+      <Content
+        align={align}
+        className={clsx(s.content, className)}
+        loop={loop}
+        ref={ref}
+        sideOffset={sideOffset}
+        {...props}
+      >
+        <Arrow asChild className={s.arrowBox}>
+          <div className={s.arrow} />
+        </Arrow>
+        {children}
+      </Content>
+    </Portal>
+  )
+)
 
 DropdownMenuContent.displayName = 'DropdownMenuContent'
 
